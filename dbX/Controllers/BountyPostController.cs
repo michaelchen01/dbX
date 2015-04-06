8b9b90aa2b6d461b91a6e628e9b834b2fe2a6a34
ameller1994 <artur.meller.1994@gmail.com>
2015-04-05 21:35:50 -0400

    added bounty post controller and view

---
+++ dbX/Controllers/BountyPostController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dbX.Controllers
+{
+    public class BountyPostController : Controller
+    {
+        // GET: BountyPost
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
