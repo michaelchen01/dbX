6afcdd80e901d0b413a78b9de8f59b9710a40b85
mtzhang <mengtingzhang@college.harvard.edu>
2015-04-06 22:11:02 -0400

    added marketplace

---
+++ dbX/Controllers/MarketplaceController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dbX.Controllers
+{
+    public class MarketplaceController : Controller
+    {
+        // GET: Marketplace
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
