8f958c0db5ebabeba2938367dc4459f86bdd88bc
michaelchen01 <michaelchen92067@gmail.com>
2015-04-13 20:42:26 -0400

    Made some Profile Changes

--- dbX/Controllers/ProfileController.cs
+++ dbX/Controllers/ProfileController.cs
@@ -3,6 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+using MongoDB.Driver.GridFS;
+using MongoDB.Driver.Linq;
+using dbX.Models;
+using dbX.Domain.Repository;
 
 namespace dbX.Controllers
 {
@@ -11,6 +18,16 @@ namespace dbX.Controllers
         // GET: Profile
         public ActionResult Index()
         {
+
+            string id = "552c5e88d4a08114c0d4a905";
+
+            UserRepository userRepository = new UserRepository();
+            User user = userRepository.GetUser(id);
+
+            ViewData["Username"] = user.Username;
+            ViewData["Coins"] = user.Coins;
+            // TODO: Artur
+
             return View();
         }
     }
