c9901c64cb707ced5966c12913ced88272582c5f
michaelchen01 <michaelchen92067@gmail.com>
2015-05-02 17:45:09 -0400

    Embedded the Ace Code editor

--- dbX/App_Start/BundleConfig.cs
+++ dbX/App_Start/BundleConfig.cs
@@ -28,6 +28,10 @@ namespace dbX
                     "~/Scripts/intro.js"
                     ));
 
+            bundles.Add(new ScriptBundle("~/bundles/ace").Include(
+                    "~/Scripts/ace/*.js"
+                    ));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css",
